d43ebece42e08c27341aeff71a7e5342e173d521
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-01-25 11:20:17 +0530

    feat: navbar

--- src/components/TheNavbar.jsx
+++ src/components/TheNavbar.jsx
@@ -1,24 +1,66 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { RiMessage2Fill } from "react-icons/ri";
+import { FaBell } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
+import logo from "../assets/img/icon.png";
+import { MdMenu } from "react-icons/md";
+import profile from "../assets/img/profile.png";
 
 const TheNavbar = () => {
-   return (
-      <nav className="flex items-center justify-between bg-gray-800 h-20 shadow-2xl">
-      <div className="logo">
-        <h1 className="text-white ml-4 cursor-pointer text-2xl">x Blog</h1>
+  return (
+    <nav className="bg-ctm3 p-5 border">
+      <div className="sm:flex sm:justify-between items-center justify-center">
+        <div className="flex justify-center items-center gap-x-2 ">
+          <img src={logo} alt="logo" className="w-8" />
+          <h3 className="font-Viga text-2xl">Blog</h3>
+        </div>
+
+        <div className="hidden md:block">
+          <ul className="flex gap-x-6 cursor-pointer">
+            <li className="hover:text-ctm5 font-bold">Home</li>
+            <li className="hover:text-ctm5 font-bold">About</li>
+            <li className="hover:text-ctm5 font-bold">Contact</li>
+          </ul>
+        </div>
+
+        {/* icons */}
+        <div className="md:px-0 flex justify-between items-center p-3 sm:p-0 ">
+          <ul className="text-ctm2 flex md:gap-x-3 gap-x-3 md:mt-0 justify-center items-center my-auto  mt-auto ">
+            <div className="relative flex items-center">
+              <div className="items-center flex ">
+                <IoSearch className="w-5 h-5 absolute  ml-2 text-gray-400 focus-within:text-gray-600 pointer-events-none" />
+                <input
+                  type="text"
+                  className="outline-none border-none rounded bg-ctm1 px-3 pr-3 pl-8 py-1 "
+                  autoComplete="off"
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+            <li className="">
+              <RiMessage2Fill className="bg-gray-400 p-1 text-3xl rounded-full hover:bg-ctm5 cursor-pointer duration-300" />
+            </li>
+            <li className="">
+              <FaBell className=" bg-gray-400 p-1 text-3xl rounded-full hover:bg-ctm5 cursor-pointer duration-300" />
+            </li>
+            <div className="cursor-pointer">
+              <img
+                src={profile}
+                alt="profile"
+                className="w-8 h-8 object-cover rounded-full "
+              />
+            </div>
+          </ul>
+
+          <div className="items-center flex">
+            <button type="button" className=" md:hidden ">
+              <MdMenu className=" bg-gray-400 p-1 text-3xl rounded-full hover:bg-ctm5 cursor-pointer duration-300 " />
+            </button>
+          </div>
+        </div>
       </div>
-      <ul className="flex">
-        <li>
-          <Link className="text-white mr-4 bg-gray-500 pt-4 p-4 pr-5 pl-5 hover:bg-gray-600 transition-all rounded" to={"/"}><i className="fas fa-home"></i> Home</Link>
-        </li>
-        <li>
-          <Link className="text-white mr-4 bg-gray-500 pt-4 p-4 pr-5 pl-5 hover:bg-gray-600 transition-all rounded" to={"/about"}><i className="fas fa-home"></i> About</Link>
-        </li>
-        <li>
-          <Link className="text-white mr-4 bg-gray-500 pt-4 p-4 pr-5 pl-5 hover:bg-gray-600 transition-all rounded" to={"/contact"}><i className="fas fa-home"></i> Contact</Link>
-        </li>
-      </ul>
     </nav>
-   )
-}
+  );
+};
 
 export default TheNavbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,14 @@ export default {
     extend: {
       fontFamily: {
         Roboto: ["Roboto Mono", "monospace"],
+        Viga: ['Viga', 'sans-serif']
       },
       colors: {
         ctm1: "#F1F6F9",
         ctm2: "#394867",
         ctm3: "#212A3E",
         ctm4: "#9BA4B5",
+        ctm5: "#00B8A9",
       },
       
     },
