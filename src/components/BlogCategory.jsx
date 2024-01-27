432050174ed70b2d4266572d2e97c2b0a0cba698
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-01-27 09:58:46 +0530

    updated: blog categories responsive

--- src/components/BlogCategory.jsx
+++ src/components/BlogCategory.jsx
@@ -3,7 +3,7 @@ const BlogCategory = () => {
     <section>
       <h5 className="pt-4 font-bold text-ctm5 text-xl">Categories</h5>
 
-      <main className="grid grid-cols-9 w-full gap-y-2 mt-4 gap-x-3">
+      <main className="grid md:grid-cols-4 2xl:grid-cols-9 grid-cols-1 w-full gap-y-2 mt-4 gap-x-3">
         
       <div className="mx-auto bg-ctm6  overflow-ellipsis overflow-hidden   hover:shadow-[0px_1.0px_0px_0px_#f7fafc] duration-200 rounded  w-full text-center">
           <button type="button" className="px-6 py-2 ">
