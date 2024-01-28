aeee48fc12248b64db33a1670dd3280e565aa380
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-01-28 13:36:44 +0530

    feat: error-page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,20 +1,36 @@
-import { Link, useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import errorImg from "../assets/img/errorimg.png";
 
 const Error = () => {
-  const { status, statusText } = useRouteError();
-  console.log(status, statusText);
-  console.log(useRouteError());
+  // const { status, statusText } = useRouteError();
+  // console.log(status, statusText);
+  // console.log(useRouteError());
 
   return (
-    <div>
-      <h4>oops Page not found</h4>
-      <div>
+    <section className="bg-ctm3 h-screen flex">
+      <div className="  mx-auto my-auto">
+        <img
+          src={errorImg}
+          alt="errorImg"
+          className="animate-bounce delay-700 duration-700"
+        />
+
+        <ul className="w-full flex md:justify-end justify-center">
         <li>
-         <Link to="/" className="text-red-500 underline">return to home Page</Link>
-        </li>
+            <Link to="/" className="bg-red-500 px-3 py-1 rounded outline-none">
+              return to homePage
+            </Link>
+          </li>
+        </ul>
       </div>
-    </div>
+    </section>
   );
 };
 
 export default Error;
+{
+  /* <li>
+<Link to="/" className="text-red-500 underline">return to home Page</Link>
+</li> */
+}
